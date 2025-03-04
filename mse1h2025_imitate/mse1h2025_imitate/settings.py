f1c3dce5854ab83522808ee6431f63153a2501c8
Shumer-1 <borisov1908vlad@mail.ru>
2025-03-04 20:32:02 +0300

    Added postgres and allowed hosts to settings

--- mse1h2025_imitate/mse1h2025_imitate/settings.py
+++ mse1h2025_imitate/mse1h2025_imitate/settings.py
@@ -17,19 +17,12 @@ from dotenv import dotenv_values
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# https://blog.gitguardian.com/how-to-handle-secrets-in-python/
-# print(open(path_join(BASE_DIR.parent, ".env"), encoding="utf-8", mode="r").read())
-SECRET_KEY = dotenv_values(path_join(BASE_DIR.parent, ".env"))["DJANGO_SECRET_KEY"]
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+config = dotenv_values(path_join(BASE_DIR.parent, ".env"))
+SECRET_KEY = config["DJANGO_SECRET_KEY"]
+ALLOWED_HOSTS = ['localhost', '0.0.0.0']
 
 # Application definition
 
@@ -80,11 +73,14 @@ WSGI_APPLICATION = 'mse1h2025_imitate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config.get("DB_NAME"),
+        'USER': config.get("DB_USER"),
+        'PASSWORD': config.get("DB_PASSWORD"),
+        'HOST': config.get("DATABASE_HOST", "db"),
+        'PORT': config.get("DATABASE_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
