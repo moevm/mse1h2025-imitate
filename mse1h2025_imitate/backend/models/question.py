f3438a721adea198d8f0f60ca735ffb464ebe891
Shumer-1 <borisov1908vlad@mail.ru>
2025-03-22 13:22:14 +0300

    Added question model and repository

---
+++ mse1h2025_imitate/backend/models/question.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.TextField()
+    category = models.CharField(max_length=100)
+    difficulty_level = models.IntegerField()
+    keywords = models.JSONField()
+
+    def __str__(self):
+        return self.question_text
