419c34819e406c39780d6ea8eca74eae2ee1a307
Shumer-1 <borisov1908vlad@mail.ru>
2025-03-23 14:32:38 +0300

    Updated service constructor and tests

--- mse1h2025_imitate/tests/test_tts_service.py
+++ mse1h2025_imitate/tests/test_tts_service.py
@@ -1,35 +1,28 @@
 import pytest
 import torch
-
 from mse1h2025_imitate.backend.text_to_speach_service import TextToSpeechService
 
+@pytest.fixture(scope='session')
+def tts_service():
+    return TextToSpeechService('ru', 'v4_ru', 'xenia')
 
 @pytest.mark.django_tts_service
-def test_tts_service_generates_audio():
-    tts = TextToSpeechService()
+def test_tts_service_generates_audio(tts_service):
     test_text = "Привет, как дела?"
-
-    audio = tts.get_speech_by_text(test_text)
-
+    audio = tts_service.get_speech_by_text(test_text)
     assert isinstance(audio, torch.Tensor), "Ожидался Tensor от PyTorch"
     assert audio.numel() > 0, "Тензор пустой"
 
-
 @pytest.mark.django_tts_service
-def test_tts_with_empty_text_should_raise():
-    tts = TextToSpeechService()
+def test_tts_with_empty_text_should_raise(tts_service):
     with pytest.raises(Exception):
-        tts.get_speech_by_text("")
-
+        tts_service.get_speech_by_text("")
 
 @pytest.mark.django_tts_service
-def test_tts_with_long_text_should_fail():
-    tts = TextToSpeechService()
-    long_text = "Привет. " * 1000  # 7000+ символов
-
+def test_tts_with_long_text_should_fail(tts_service):
+    long_text = "Привет. " * 1000
     with pytest.raises(Exception, match="too long"):
-        tts.get_speech_by_text(long_text)
-
+        tts_service.get_speech_by_text(long_text)
 
 @pytest.mark.parametrize("text", [
     "Привет!",
@@ -37,8 +30,6 @@ def test_tts_with_long_text_should_fail():
     "Сегодня хорошая погода",
     "Django — это ужасы"
 ])
-def test_tts_various_phrases(text):
-    tts = TextToSpeechService()
-    audio = tts.get_speech_by_text(text)
+def test_tts_various_phrases(tts_service, text):
+    audio = tts_service.get_speech_by_text(text)
     assert audio.numel() > 0
-
