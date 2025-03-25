c94d781bc155aafd63b3eb99656ec50f9cee6b14
kiyro7 <92889789+kiyro7@users.noreply.github.com>
2025-03-25 23:18:28 +0300

    register.html ready, changeg settings.py (authentication) for easy debug

--- mse1h2025_imitate/mse1h2025_imitate/settings.py
+++ mse1h2025_imitate/mse1h2025_imitate/settings.py
@@ -60,8 +60,9 @@ AUTHENTICATION_BACKENDS = [
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  
