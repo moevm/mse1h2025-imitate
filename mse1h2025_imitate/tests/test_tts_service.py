7d1116792c3c1e418afa2d6898d581d3caf3388a
Shumer-1 <borisov1908vlad@mail.ru>
2025-03-25 19:16:14 +0300

    Updated test names

--- mse1h2025_imitate/tests/test_tts_service.py
+++ mse1h2025_imitate/tests/test_tts_service.py
@@ -7,19 +7,19 @@ def tts_service():
     return TextToSpeechService('ru', 'v4_ru', 'xenia')
 
 @pytest.mark.django_tts_service
-def test_tts_service_generates_audio(tts_service):
+def test_tts_service_must_return_not_empty_tensor(tts_service):
     test_text = "Привет, как дела?"
     audio = tts_service.get_speech_by_text(test_text)
     assert isinstance(audio, torch.Tensor), "Ожидался Tensor от PyTorch"
     assert audio.numel() > 0, "Тензор пустой"
 
 @pytest.mark.django_tts_service
-def test_tts_with_empty_text_should_raise(tts_service):
+def test_tts_service_must_raise_if_empty_text(tts_service):
     with pytest.raises(Exception):
         tts_service.get_speech_by_text("")
 
 @pytest.mark.django_tts_service
-def test_tts_with_long_text_should_fail(tts_service):
+def test_tts_service_must_fail_if_too_long_text(tts_service):
     long_text = "Привет. " * 1000
     with pytest.raises(Exception, match="too long"):
         tts_service.get_speech_by_text(long_text)
@@ -30,6 +30,6 @@ def test_tts_with_long_text_should_fail(tts_service):
     "Сегодня хорошая погода",
     "Django — это ужасы"
 ])
-def test_tts_various_phrases(tts_service, text):
+def test_tts_service_must_return_not_empty_tensor_if_varius_phrases(tts_service, text):
     audio = tts_service.get_speech_by_text(text)
     assert audio.numel() > 0
