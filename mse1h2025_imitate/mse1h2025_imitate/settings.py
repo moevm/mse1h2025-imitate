b69bcc4d21f17f21d87df031daca7982921f424f
kiyro7 <92889789+kiyro7@users.noreply.github.com>
2025-03-25 15:18:28 +0300

    registration page almost ready, moved images in nested folder

--- mse1h2025_imitate/mse1h2025_imitate/settings.py
+++ mse1h2025_imitate/mse1h2025_imitate/settings.py
@@ -17,6 +17,8 @@ from dotenv import dotenv_values
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [path_join(BASE_DIR, 'static')]
 
 DEBUG = True
 
