956152150474a5e66702eb6a48d99c16d35e059f
kiyro7 <92889789+kiyro7@users.noreply.github.com>
2025-04-01 17:29:05 +0300

    question repository small changes

--- mse1h2025_imitate/backend/repositories/question_repository.py
+++ mse1h2025_imitate/backend/repositories/question_repository.py
@@ -1,18 +1,22 @@
 from django.db.models import Q
 from ..models.question import Question
+from functools import reduce
+from operator import and_
 
 
 class QuestionRepository:
     @staticmethod
     def get_questions_by_keywords_any(keywords):
+        if not keywords:
+            return Question.objects.none()  # Возвращаем пустой QuerySet, если нет ключевых слов
         query = Q()
         for kw in keywords:
-            query |= Q(keywords__contains=[kw])
+            query |= Q(keywords__contains=kw)  # Убираем квадратные скобки
         return Question.objects.filter(query)
 
     @staticmethod
     def get_questions_by_keywords_all(keywords):
-        qs = Question.objects.all()
-        for kw in keywords:
-            qs = qs.filter(keywords__contains=[kw])
-        return qs
+        if not keywords:
+            return Question.objects.none()  # Возвращаем пустой QuerySet, если нет ключевых слов
+        query = reduce(and_, (Q(keywords__contains=kw) for kw in keywords))  # через reduce и убрали квадратные скобки
+        return Question.objects.filter(query)
