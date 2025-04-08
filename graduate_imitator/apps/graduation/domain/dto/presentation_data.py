49ec51e1d4850643987664721d03be554f677ffd
srezal <sereshka0967@gmail.com>
2025-04-08 21:19:18 +0300

    changed ParserData from a dataclass to child class of pydantic.BaseModel

--- graduate_imitator/apps/graduation/domain/dto/presentation_data.py
+++ graduate_imitator/apps/graduation/domain/dto/presentation_data.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 from typing import List
+from pydantic import BaseModel
 
 
-@dataclass
-class PresentationData:
+class PresentationData(BaseModel):
     topic: str
     goalAndTasks: str
     author: str
