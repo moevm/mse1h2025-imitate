ccf631fd6d2db3ea59bb24243113799f19c752c9
kiyro7 <92889789+kiyro7@users.noreply.github.com>
2025-04-10 01:35:04 +0300

    added endpoint for user results and js to display them on page

--- graduate_imitator/apps/graduation/interfaces/web/views.py
+++ graduate_imitator/apps/graduation/interfaces/web/views.py
@@ -32,9 +32,12 @@ class LoginWebView(View):
 
 class ProfileWebView(View):
     def get(self, request):
-        context = {
-            "results": ["first_result", "second_result"]
-        }
+        context = {}
+        if 'context_for_front' in request.GET:
+            try:
+                context = loads(request.GET['context_for_front'])
+            except JSONDecodeError:
+                context = {}
         return render(request, "user/profile.html", context)
 
 
@@ -44,7 +47,7 @@ class ProfileWebView(View):
 
 from ...domain.repositories.user_repository import UserRepository
 from ...domain.repositories.presentation_repository import PresentationRepository
-from ...domain.repositories.answer_repository import AnswerRepository
+from ...domain.repositories.attempt_repository import AttemptRepository
 from datetime import datetime
 from random import randint
 
@@ -54,4 +57,11 @@ now1 = datetime.now()
 now2 = datetime.now()
 presentation = PresentationRepository.get_presentations_by_user_id(user)[0]
 AttemptRepository.create_attempt(user, presentation, now1, now2, randint(0, 100), True)
+
+# получение защит 
+user = UserRepository.get_user_by_id(request.user.id)
+results = AttemptRepository.get_attempts_by_user_id(user)
+if len(results) > 10:
+    for r in results:
+        AttemptRepository.delete_attempt(r.id)
 """
